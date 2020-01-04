11826698190731aea8c2e0ad8cd063a8b4acb0ce
Chris Lopez <clmg514@gmail.com>
2020-01-04 08:58:20 -0800

    Updates

---
+++ precache-manifest.08e18cbddb352658ae4af3ddab803bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306b72ea6b5182b13168d83d04ebb792",
+    "url": "/budgetplanner/index.html"
+  },
+  {
+    "revision": "ce74ba73d14ecc434caa",
+    "url": "/budgetplanner/static/css/main.82d56b65.chunk.css"
+  },
+  {
+    "revision": "02d2c39e71f1d8283cc0",
+    "url": "/budgetplanner/static/js/2.c5318d1b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/budgetplanner/static/js/2.c5318d1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce74ba73d14ecc434caa",
+    "url": "/budgetplanner/static/js/main.db188056.chunk.js"
+  },
+  {
+    "revision": "e6fd540c372774a8b850",
+    "url": "/budgetplanner/static/js/runtime-main.d1a87052.js"
+  }
+]);
